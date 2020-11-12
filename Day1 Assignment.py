3284d7c0fa4efb5678d11f710e3d2412174c53b5
Nensi02 <74348679+Nensi02@users.noreply.github.com>
2020-11-12 14:44:35 +0530

    Assignment Day1 submitted

---
+++ Day1 Assignment.py
@@ -0,0 +1,37 @@
+# 1. Given the following jumbled word, OBANWRI guess the correct English word.
+
+A. RANIBOW
+B. RAINBOW
+C. BOWRANI
+D. ROBWANI
+
+ANS: 
+    B. RAINBOW
+
+# 2. Write a Program which prints "LETS UPGRADE"
+
+str="LETS UPGRADE"
+print(str)
+
+# 3. write a program that takes cost price and Selling price as input and displays whether the transaction is a Profit or a Loss or Neither.  
+
+
+Cost= int(input())
+Sell= int(input())
+
+if Sell>Cost:
+    print("Profit")
+elif Sell<Cost:
+    print("Loss")
+else:
+    print("Neither")
+
+# 4. Write a program that takes an amount in Euros as input. You need to find its equivalent in Rupees and display it. Assume 1 Euro equals Rs. 80. 
+
+Euros= int(input( ))
+
+Rupees=Euros*80
+
+print(Rupees)
+
+# END OF ASSIGNMENT-1
